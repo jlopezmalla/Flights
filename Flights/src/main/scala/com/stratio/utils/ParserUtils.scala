3fb079a0f6afcdb1c06adcd570fa9df2149e5dec
Luis Diaz <ldifernan@gmail.com>
2015-04-09 11:39:44 +0200

    added new parsed function

--- Flights/src/main/scala/com/stratio/utils/ParserUtils.scala
+++
@@ -1,9 +0,0 @@
-package com.stratio.utils
-
-import org.joda.time.DateTime
-
-object ParserUtils {
-
-  def getDateTime(year: Int, month: Int, day: Int): DateTime =
-    new DateTime().year().setCopy(year).monthOfYear().setCopy(month).dayOfMonth().setCopy(day)
-}
--- src/main/scala/com/stratio/utils/ParserUtils.scala
+++ src/main/scala/com/stratio/utils/ParserUtils.scala
@@ -2,8 +2,18 @@ package com.stratio.utils
 
 import org.joda.time.DateTime
 
+import scala.util.{Failure, Try, Success}
+
 object ParserUtils {
 
   def getDateTime(year: Int, month: Int, day: Int): DateTime =
     new DateTime().year().setCopy(year).monthOfYear().setCopy(month).dayOfMonth().setCopy(day)
+
+  def parseInt(s: String): Option[Int] = {
+    val v =Try{ s.toInt}
+    v match {
+      case Success(i) => Some(i)
+      case Failure => None
+    }
+  }
 }

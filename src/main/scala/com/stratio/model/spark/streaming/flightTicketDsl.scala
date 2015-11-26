55bd5c325843e8fc259807456c2a047c0ce863ff
Jorge Lopez-Malla <jlopezm@stratio.com>
2015-11-26 15:56:01 +0100

    streaming test

--- src/main/scala/com/stratio/model/spark/streaming/flightTicketDsl.scala
+++ src/main/scala/com/stratio/model/spark/streaming/flightTicketDsl.scala
@@ -8,25 +8,25 @@ import org.apache.spark.streaming.dstream.DStream
 
 class FlightTicketFunctions(self: DStream[FlightTicket]){
 
-  def userAvgPersonalPayerByWindow(flights: RDD[Flight], windowSeconds: Int): DStream[(String, Float)]= {
-    byAirport(flights).mapValues(_ => (1f, 1f)).reduceByKeyAndWindow(
-        (acc, newValue) => ((acc._1 * acc._2) / (newValue._1 * newValue._2), acc._2 + newValue._2),
-        Seconds(windowSeconds)).map(tuple => (tuple._1, tuple._2._1))
-  }
 
-  def avgAgeByWindow(windowSeconds: Int, slideSeconds: Int): DStream[(String, Float)]= {
-    self.map(ticket => (ticket.passenger.age.toString, 1f)).reduceByWindow(
-      (acc, newValue) => (acc._1 +";"+ newValue._1, acc._2 + newValue._2), Seconds(windowSeconds), Seconds(slideSeconds))
-//      (acc, newValue) => (((acc._1 * acc._2) + (newValue._1 * newValue._2))/(acc._2 + newValue._2), acc._2 + newValue._2),
-      //.map(_._1)
+  def avgAgeByWindow(windowSeconds: Int, slideSeconds: Int): DStream[(Float, Float)]= {
+    self.map(ticket => (ticket.passenger.age.toFloat, 1f)).reduceByWindow(
+    (value1, value2) => (((value1._1 * value1._2) + (value2._1 * value2._2))/(value1._2 + value2._2),
+    value1._2 + value2._2), Seconds(windowSeconds), Seconds(slideSeconds))
   }
-
 
   def byAirport(flights: RDD[Flight]): DStream[(String, FlightTicket)] = {
     val airportFNum = flights.map(flight => (flight.flightNum, flight.origin))
     self.map(ticket => (ticket.flightNumber, ticket)).
       transform(ticketNumber => ticketNumber.join(airportFNum)).map(tuple => (tuple._2._2, tuple._2._1))
   }
+
+  def airportMaxFlightsByWindow(flights: RDD[Flight], windowSeconds: Int): DStream[(String, Int)]= {
+    byAirport(flights).mapValues(_ => 1).reduceByKeyAndWindow(_ + _, Seconds(windowSeconds))
+//      .reduce((airportCounter1, airportCounter2) =>
+//    if(airportCounter1._2 > airportCounter2._2) airportCounter1 else airportCounter2)
+  }
+
 
   def airportStatistics(flights: RDD[Flight]): DStream[(String, AirportStatistics)]= {
     byAirport(flights).updateStateByKey((tickets: Seq[FlightTicket], airportStatistic: Option[AirportStatistics]) => {
@@ -43,8 +43,8 @@ class FlightTicketFunctions(self: DStream[FlightTicket]){
 
 trait FlightTicketDsl {
 
-  implicit def ticketFunctions(tickets: DStream[FlightTicket]):
-    FlightTicketFunctions = new FlightTicketFunctions(tickets)
+  implicit def ticketFunctions(tickets: DStream[FlightTicket]): FlightTicketFunctions =
+    new FlightTicketFunctions(tickets)
 }
 
 object FlightTicketDsl extends FlightTicketDsl
--- src/test/scala/com/stratio/model/spark/streaming/FlightTicketStreamingTest.scala
+++ src/test/scala/com/stratio/model/spark/streaming/FlightTicketStreamingTest.scala
@@ -33,6 +33,7 @@ class FlightTicketStreamingTest extends StreamingSuiteBase {
       cancellationCode= 0,
       delay= delays1)
     val flight2 = flight1.copy(flightNum = 2, origin= "SAN", dest= "SFO")
+    val flight3 = flight1.copy(flightNum = 3, origin= "JFK", dest= "SFO")
 
     val flights = sc.parallelize(List(flight1, flight2))
   }
@@ -42,21 +43,59 @@ class FlightTicketStreamingTest extends StreamingSuiteBase {
     val passenger1 = Person("Jorge", 'H', 30, Some(750.5f))
     val passenger2 = Person("Maria", 'F', 50, Some(1250.0f))
     val passenger3 = Person("Gaspar", 'H', 12)
+    val passenger4 = Person("Julia", 'F', 25)
     val ticket11 = FlightTicket(1, passenger1, Company)
-    val ticket12 = ticket11.copy(passenger = passenger2, payer = Personal)
-    val ticket13 = ticket11.copy(passenger = passenger3)
-    val ticket21 = FlightTicket(2, passenger1, Personal)
-    val ticket22 = ticket21.copy(passenger = passenger2, payer = Personal)
-    val flight1Tickets = List(List(), List(ticket11), List(ticket22),
-      List(ticket13, ticket21), List(ticket12), List(), List(ticket12), List(ticket13, ticket21))
+    val ticket12 = ticket11.copy(passenger = passenger2)
+    val ticket13 = ticket11.copy(passenger = passenger3, payer = Personal)
+    val ticket21 = FlightTicket(2, passenger1, Company)
+    val ticket22 = ticket21.copy(passenger = passenger2)
+    val ticket23 = ticket21.copy(passenger = passenger3, payer = Personal)
+    val ticket24 = ticket21.copy(passenger = passenger4, payer = Personal)
+    val ticket31 = FlightTicket(3, passenger1, Personal)
+    val ticket32 = ticket31.copy(passenger = passenger4)
 
+    val flightTickets = List(List(ticket11, ticket12), List(), List(ticket13, ticket21), List(ticket12), List(ticket12),
+      List(ticket13, ticket21, ticket24), List(), List(ticket23))
+
+    val expectedFlightByAirport = List(List((30.5f, 4.0f)), List((50.0f, 2.0f)), List((19.75f, 4.0f)))
+
+    val expectedFlightByAirportSolution = List(
+      List(("SFO", 3)),
+      List(("SFO", 2)),
+      List(("SAN", 2)), List())
   }
 
-  test("calculate the ticket average by airport each 2 Seconds with a 4 second slide") {
+  test("calculate the age average each 3 Seconds with a 3 second slide") {
     import FlightTicketDsl._
-    val input = WithQueuedRDD.flight1Tickets
-    //    val expected = input.map(_.map(_.toString()))
-    val expected = List(List(("1.0f", 2.0f)), List(), List())
-    testOperation(input, (s: DStream[FlightTicket]) => s.avgAgeByWindow(3, 2), expected, 7, false)
+    val input = WithQueuedRDD.flightTickets
+    val expected = WithQueuedRDD.expectedFlightByAirport
+    testOperation(input,
+      (s: DStream[FlightTicket]) => s.avgAgeByWindow(3, 3),
+      expected,
+      9,
+      false)
+  }
+
+  test("group flights by aiport in each micro batch") {
+    import FlightTicketDsl._
+    val input = WithQueuedRDD.flightTickets
+    val expected = List(List(), List(), List() ,List() ,List(), List())
+    testOperation(input,
+      (s: DStream[FlightTicket]) => s.byAirport(WithFlights.flights),
+      expected,
+      7,
+      false)
+  }
+
+
+  test("get max flight every 3 seconds") {
+    import FlightTicketDsl._
+    val input = WithQueuedRDD.flightTickets
+    val expected = List(List(), List(), List(), List(), List())
+    testOperation(input,
+      (s: DStream[FlightTicket]) => s.airportMaxFlightsByWindow(WithFlights.flights, 2),
+      expected,
+      7,
+      false)
   }
 }
